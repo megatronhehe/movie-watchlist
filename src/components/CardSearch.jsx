21f22bf4c80bc76bc0bc4563bbc0cfe40dd45645
Megatronhehee <satyamahendra09@gmail.com>
2023-07-29 15:12:37 +0700

    fix: fix alittle bit more ui issues on movie cards in search movie section

--- src/components/CardSearch.jsx
+++ src/components/CardSearch.jsx
@@ -31,8 +31,8 @@ const CardSearch = ({
 	const [toggleShow, setToggleShow] = useState(false);
 
 	return (
-		<div className="relative flex gap-2 mt-8 text-gray-200 rounded-md">
-			<img src={poster} alt="" className="w-full rounded-lg " />
+		<div className="relative flex gap-2 overflow-hidden text-gray-200 rounded-xl">
+			<img src={poster} alt="" className="w-full " />
 			{toggleShow && (
 				<div className="absolute w-full h-full p-4 overflow-y-scroll text-sm bg-black bg-opacity-80">
 					<div className="text-center ">
@@ -87,61 +87,18 @@ const CardSearch = ({
 			>
 				{toggleShow ? <BsChevronUp /> : <BsChevronDown />}
 			</button>
-			<p className="absolute flex items-center gap-2 p-2 font-semibold text-white bg-gray-700 rounded-md bottom-2 left-2 bg-opacity-80">
-				<BsStarFill className="text-yellow-400" />
-				{imdbRating}
-			</p>
+
+			{!toggleShow && (
+				<div className="absolute bottom-0 flex items-center justify-between w-full p-2">
+					<p className="flex items-center gap-2 p-2 text-xl font-semibold text-white bg-gray-700 rounded-md bg-opacity-80">
+						<BsStarFill className="text-yellow-400" />
+						{imdbRating}
+					</p>
+					<p className="px-4 py-2 text-sm bg-black rounded-full">{type}</p>
+				</div>
+			)}
 		</div>
 	);
 };
-
-{
-	/* <div
-			key={imdbID}
-			className="relative flex p-2 mb-4 text-xs tracking-wide text-blue-700 bg-gray-200 rounded-lg shadow-lg"
-		>
-			<img
-				src={poster}
-				className="object-cover w-1/3 h-full bg-gray-200 rounded-lg shadow-md "
-			></img>
-			<div className="w-full ml-4">
-				<h1 className="pb-3 mb-2 font-bold text-center border-b border-blue-500 text-md">
-					{title} <span className="">({year})</span>
-				</h1>
-				<div className="flex items-center justify-between gap-2">
-					<ul className="">
-						<li className="flex items-center justify-center gap-2">
-							<BsStarFill color="orange" />
-							{imdbRating}
-						</li>
-						<li className="flex items-center gap-2">
-							<BsClockFill />
-							{runtime}
-						</li>
-						<li className="flex items-center gap-2">
-							<BsFillCollectionPlayFill />
-							{genre}
-						</li>
-						<li className="flex gap-2">
-							<BsFillPeopleFill />
-							{actors}
-						</li>
-						<li className="pt-2 mt-2 border-t border-gray-400">{plot}</li>
-					</ul>
-				</div>
-			</div>
-			<button
-				onClick={() => addWatchlist(imdbID)}
-				className="px-2 ml-2 text-xl text-blue-500 bg-gray-100 border border-blue-500 rounded-lg"
-			>
-				+
-			</button>
-			{isExistInWatchlist(imdbID) && (
-				<p className="absolute top-0 flex items-center gap-2 p-2 text-xs text-blue-100 bg-blue-500 rounded-b-lg left-4">
-					Listed In Watchlist <BsFillClipboard2CheckFill />
-				</p>
-			)}
-		</div> */
-}
 
 export default CardSearch;
