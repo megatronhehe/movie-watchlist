88ee425cd2ec7844bc360e4af88ed7072d828839
Megatronhehee <satyamahendra09@gmail.com>
2023-08-02 15:14:44 +0700

    fix: ui fixes poster overlapping or whatever

--- src/components/PosterModal.jsx
+++ src/components/PosterModal.jsx
@@ -6,9 +6,9 @@ const PosterModal = ({ poster, setTogglePosterModal }) => {
 	return (
 		<div
 			onClick={() => setTogglePosterModal(false)}
-			className="fixed top-0 left-0 flex flex-col items-center justify-center w-full h-full gap-4 bg-gray-500 bg-opacity-50"
+			className="fixed top-0 left-0 z-50 flex flex-col items-center justify-center w-full h-full gap-4 bg-gray-500 bg-opacity-50"
 		>
-			<button className="flex items-center justify-center w-12 h-12 bg-white rounded-full">
+			<button className="flex items-center justify-center w-12 h-12 text-2xl text-white bg-gray-300 rounded-full hover:bg-white hover:text-red-400">
 				<BsX />
 			</button>
 			<img
